f250698b44ca541ceea856ff47168104753f0a01
devkdn <5129408-devkdn@users.noreply.gitlab.com>
2021-08-05 18:07:38 -0500

    Add function to parseLectureName

--- src/time.ts
+++ src/time.ts
@@ -1,9 +1,25 @@
 import { Section } from "./sections";
 const { ms, s, m, h, d } = require('time-convert');
 
-const regex = /( \((?<minutes>\d{1,2}):(?<seconds>\d{2})\))/;
+export interface LectureObject {
+    title: string | undefined,
+    seconds: number
+}
 
-const calculateSeconds = (seconds: number, minutes = 0) => (minutes * 60) + seconds;
+const regex = /(?<title>.*)( \((?<minutes>\d{1,2}):(?<seconds>\d{2})\))/;
+
+const calculateSeconds = (seconds = 0, minutes = 0) => (minutes * 60) + seconds;
+
+export const parseLectureName = (lectureName: string) : LectureObject => {
+    const lectureRegex = lectureName.match(regex)?.groups;
+    const lectureTitle = lectureRegex?.title;
+    const lectureMinutes = lectureRegex?.minutes ? parseInt(lectureRegex?.minutes) : 0;
+    const lectureSeconds = lectureRegex?.seconds ? parseInt(lectureRegex?.seconds) : 0;
+    
+    const totalSeconds = calculateSeconds(lectureSeconds, lectureMinutes);
+
+    return {title: lectureTitle, seconds: totalSeconds};
+}
 
 export const calculateTimeLeftInCourse = (sections: Section[]) => {
     let minutesLeftInCourse = 0;
@@ -37,9 +53,9 @@ export const calculateTimeLeftInSection = (section: Section) => {
 
 export const getHumanTimeLeft = (seconds: number) => {
     const milliseconds = ms.from(s)(seconds);
-    const time = ms.to(d,h,m,s)(milliseconds);
+    const time = ms.to(d, h, m, s)(milliseconds);
     if (time[3] >= 30) {
-        time[2]++; 
+        time[2]++;
     }
     const days: string = time[0] ? `${time[0]}d` : '';
     const hours: string = time[1] ? `${time[1]}h` : '';
